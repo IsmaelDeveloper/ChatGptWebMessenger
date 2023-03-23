ad4fe77e8db1843aceff155d1c0da4ab292ff587
ismaelDeveloper <ismael.hadj13@gmail.com>
2023-03-23 12:07:49 +0900

    api key

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
-const API_KEY = "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";
+const API_KEY = "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";
 // "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
   "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
